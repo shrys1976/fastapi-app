59352005be489bdc8afbb01e03a9c2c48be741af
shrys <shreyasjain2018@gmail.com>
2026-02-13 20:56:54 +0530

    add html frontend

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-app =  FastAPI()
 
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
@@ -27,10 +30,15 @@ posts: list[dict] = [
 
 
 
-@app.get("/posts",include_in_schema=False) # include in schema disables route visibility in fastapidocs
-@app.get("/",include_in_schema=False)# response class changed to html since originally its json
-def home(request:Request):
-    return templates.TemplateResponse(request,"home.html",{"posts":posts}) # now we can write html since response is changed
+@app.get("/posts", include_in_schema=False,name = "posts")  # include in schema disables route visibility in fastapidocs
+@app.get("/", include_in_schema=False,name  = "home")  # response class changed to HTML since originally it's JSON
+def home(request: Request) -> HTMLResponse:
+    context = {
+        "request": request,
+        "posts": posts,
+        "title": "Home",
+    }
+    return templates.TemplateResponse("home.html", context)
 
 # we dont hide this from docs since its json route
 @app.get("/api/posts")
